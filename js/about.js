4f13728f33854924a32bb9bf6dfb71626a516c5e
smriti.mool <mool.smreeti@gmail.com>
2022-08-13 18:01:01 -0400

    update about.js

--- js/about.js
+++ js/about.js
@@ -1,5 +1,6 @@
+//Accordion/toggle company vision, Mission and values section.
 
-let accordion = document.getElementsByClassName("accordion");
+let accordion = document.getElementsByClassName("accordion"); // for collection of item 
 let i;
 
 for (i = 0; i < accordion.length; i++) {
@@ -10,26 +11,30 @@ for (i = 0; i < accordion.length; i++) {
 
         /* Toggle between hiding and showing the active panel */
         let panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
+        if (panel.style.display === "block")//when we click on panel it display items in block section 
+        {
             panel.style.display = "none";
-        } else {
+        }
+        else {
             panel.style.display = "block";
         }
     });
 }
 
 /*testimonials section*/
-
 const clientTabItems = document.querySelectorAll('.client-item');
 const showDiv = document.querySelector('.show-info');
 
+//take input when we click on the item 
 clientTabItems.forEach((item) => {
     item.addEventListener('click', () => {
         showInfo(item);
     });
 });
 
+
 function showInfo(item) {
+    // finding elements using queryselector and display the items one by one when click on particular item 
     showDiv.querySelector('.show-img img').src = item.querySelector('.client-thumbnail img').src;
     showDiv.querySelector('.show-name').innerHTML = item.querySelector('.client-name').innerHTML;
     showDiv.querySelector('.show-designation').innerHTML = item.querySelector('.client-designation').innerHTML;
